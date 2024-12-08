424b6158a84b082ff1fdb08d5b4b08015fe65ebf
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2024-12-08 20:36:52 +0530

    added vercel.json

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
-// Imports
 const express = require('express');
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 const { connectDb } = require('./Config/dbConnection');
 const todoRoutes = require('./Routes/todoRoutes');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: false
+};
 
 // Middlewares
-app.use(cors()); 
+app.use(cors(corsOptions));
 app.use(express.json());
-
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running on port 3000');
-});
 
 // mount todo routes
 app.use('/todos/', todoRoutes);
 
 // Connecting to mongodb atlas
 connectDb();
+
+// For prod testing
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Remove the app.listen() call for Vercel deployment
+module.exports = app;
+
+
+// For Dev 
+// app.listen(process.env.PORT, ()=>{
+//     console.log('Server is running on port 3000');
+// });
