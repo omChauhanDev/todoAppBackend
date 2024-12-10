8625ddc94e2b75a659f4c52ecc99bad56efe46e5
Om Chauhan <omchauhan@Oms-MacBook-Pro.local>
2024-12-10 17:58:17 +0530

    fixed type to commonjs

--- Config/dbConnection.js
+++ Config/dbConnection.js
@@ -1,12 +1,15 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 dotenv.config();
 // to connect to mongodb
-export const connectDb = async () =>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log('successfully connected to mongodb');
-    } catch(error){
-        console.log('Error connecting to mongodb', error);
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("successfully connected to mongodb");
+  } catch (error) {
+    console.log("Error connecting to mongodb", error);
+  }
+};
+
+module.exports = { connectDb };
--- Controller/todoController.js
+++ Controller/todoController.js
@@ -1,41 +1,43 @@
-import Todo from '../Models/Todo.js';
+const Todo = require("../Models/Todo");
 
-export const createTodo = async (req, res)=>{
-    const currentTodo = req.body;
-    const newTodo = { id: Date.now(), title: req.body.title, completed: false };
-    await Todo.create(newTodo);
-    const updatedTodos = await Todo.find();
-    res.send({
-        status: 200,
-        message: 'Todo created successfully',
-        data: updatedTodos
-    })
-}
+const createTodo = async (req, res) => {
+  const currentTodo = req.body;
+  const newTodo = { id: Date.now(), title: req.body.title, completed: false };
+  await Todo.create(newTodo);
+  const updatedTodos = await Todo.find();
+  res.send({
+    status: 200,
+    message: "Todo created successfully",
+    data: updatedTodos,
+  });
+};
 
-export const getAllTodos = async (req, res)=>{
-    const allTodos = await Todo.find();
-    res.send(allTodos);
-}
+const getAllTodos = async (req, res) => {
+  const allTodos = await Todo.find();
+  res.send(allTodos);
+};
 
-export const deleteTodo = async (req, res)=>{
-    const id = req.params.id;
-    await Todo.findByIdAndDelete(id);
-    const updatedTodos = await Todo.find();
-    res.send({
-        status: 200,
-        message: 'Todo deleted successfully',
-        data: updatedTodos
-    })
-}
+const deleteTodo = async (req, res) => {
+  const id = req.params.id;
+  await Todo.findByIdAndDelete(id);
+  const updatedTodos = await Todo.find();
+  res.send({
+    status: 200,
+    message: "Todo deleted successfully",
+    data: updatedTodos,
+  });
+};
 
-export const updateTodo = async (req, res)=>{
-    const id = req.params.id;
-    const updatedTodo = req.body;
-    await Todo.findByIdAndUpdate(id, updatedTodo);
-    const updatedTodos = await Todo.find();
-    res.send({
-        status: 200,
-        message: 'Todo updated successfully',
-        data: updatedTodos
-    })
-}
+const updateTodo = async (req, res) => {
+  const id = req.params.id;
+  const updatedTodo = req.body;
+  await Todo.findByIdAndUpdate(id, updatedTodo);
+  const updatedTodos = await Todo.find();
+  res.send({
+    status: 200,
+    message: "Todo updated successfully",
+    data: updatedTodos,
+  });
+};
+
+module.exports = { createTodo, updateTodo, getAllTodos, deleteTodo };
